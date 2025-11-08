d7eb298a965057b218947cd79176abc24ee93508
Shraddha Shinde <90777760+ShraddhaBShinde@users.noreply.github.com>
2025-11-08 02:07:27 +0000

    scope done

--- Restart/Scope.js
+++ Restart/Scope.js
@@ -11,3 +11,47 @@ if (true) {
 console.log(a);
 //console.log(b); // will not run as b will be not identified outside the block scope as it is declared inside the block
 console.log(c);
+
+//Scope level and mini hoisting in javascript
+function one(){
+    const username = "shraddha"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); //can't access as it is outside the scope
+
+     two()
+
+}
+
+one() //o/p-> shraddha  (concept of closure)
+
+if (true) {
+    const username = "shraddha"
+    if (username === "shraddha") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    //console.log(website); //gives error as not defined
+}
+
+//console.log(username); //gives error as not defined
+
+
+// ++++++++++++++++++ Hoisting ++++++++++++++++++
+
+
+console.log(addone(5)) //this runs without error and gices o/p->6
+
+function addone(num){
+    return num + 1
+} 
+
+
+
+// addTwo(5) //gives error cannot access "addTwo" before initialization
+// const addTwo = function(num){
+//     return num + 2
+// }
